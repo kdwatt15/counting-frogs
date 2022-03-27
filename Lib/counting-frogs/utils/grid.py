11e8971da8c2db19a92e438050d156dc496b7078
Kevin Watt <kevin.watt@ey.com>
2022-03-26 21:07:34 -0500

    Adding utils and comments

--- Lib/counting-frogs/utils/grid.py
+++ Lib/counting-frogs/utils/grid.py
@@ -1,19 +1,38 @@
 # Grid class for generating a dataframe that contains a coordinate grid
 # pypi imports
 import pandas as pd
-from numpy import average
 import seaborn as sns
-import multiprocessing.pool
 
 class grid:
+    """
+    This class takes the corner coordinates of a rectangle and returns a dataframe with all the coorindates 
+    inbetween given level of decimal precision specified
+    """
 
     def __init__(self, decimal_degrees=0, square_coords=(-5, 5, -10, 10)):
+        """
+        Initialize an instance of the grid class
+        :inputs:
+        - decimal_degress - int - decimal degrees of precision (n >= 0)
+        - square_coords - list - list of float / int representing the bottom left and top right coordinates of a rectangle
+        """
         self.square_coords = square_coords
         self.coords = self._build_coords(decimal_degrees, square_coords[0], square_coords[1], square_coords[2], square_coords[3])
         self.grid = self._build_grid(self.coords)
         sns.set(rc={'figure.facecolor':'cornflowerblue'})
 
     def _build_coords(self, decimal_degrees, min_lat, max_lat, min_long, max_long):
+        """
+        Builds a dictionary of coordinates to later be used to create a pandas.DataFrame
+        :inputs:
+        - decimal_degress - int - decimal degrees of precision (n >= 0)
+        - min_lat - int - southwesterly most left latitude
+        - max_lat - int - northeasterly most right latitude
+        - min_long - int - southwesterly most longitude
+        - max_long - int - northeasterly most longitude
+        :output:
+        - dict - dictionary of latitudes and longitudes
+        """
         if decimal_degrees == 0:
             increment = 1
         else:
@@ -39,14 +58,28 @@ class grid:
         return coords
 
     def _build_grid(self, coords):
+        """ 
+        Creates a pandas.DataFrame with coordinate pairs as the index
+        :input:
+        - coords - dict - output of the _build_coords function
+        :output:
+        pandas.DataFrame - df with lat, long, and a string index of the coordinate pairs
+        """
         df = pd.DataFrame(coords)
         df["coord"] = df["lat"].astype(str) + ", " + df["long"].astype(str)
         df.set_index("coord", inplace=True)
         return df
         
     def show_coords(self):
+        """
+        Generate a scatterplot two show the points generated on the grid
+        :inputs:
+        - None.
+        :output:
+        - None.
+        """
         sns.scatterplot(data=self.coords, x="long", y="lat")
 
 if __name__ == "__main__":
     grid = grid(decimal_degrees=1, square_coords=(-5, 5, -10, 10))
-    grid.show_grid()
+    grid.show_coords()
--- Lib/counting-frogs/utils/utils.py
+++ Lib/counting-frogs/utils/utils.py
@@ -1,13 +1,35 @@
-# general utitities
+# Utilities used to support the frog counting challenge
+# Standard imports
+from math import radians, cos, sin, asin, sqrt
+
+# Project imports
 from .grid import grid
 
 def build_grid(df, decimal_degrees):
+    """
+    Takes a dataframe with "lat" and "long" and initializes a grid object
+    :inputs:
+    - df - pandas.DataFrame - needs to have the columns "lat" and "long"
+    - decimal_degrees - int -  with the number of decimal degrees of precision to be applied to the coordinates
+    :output:
+    - grid - (see ~\\utils\\grid.py for more details)
+    """
     lat_min, lat_max = df["lat"].min(), df["lat"].max()
     long_min, long_max = df["long"].min(), df["long"].max()
 
     return grid(decimal_degrees=decimal_degrees, square_coords=(lat_min, lat_max, long_min, long_max))
 
 def count_frogs(df, degrees, lat_col, long_col):
+    """
+    Takes a dataframe of frog observances and returns the number of observances at the specified decimal degree of precision
+    :inputs:
+    - df - pandas.DataFrame - frog observances
+    - degrees - int - representing the decimal degrees of precision for coordinates
+    - lat_col - str - name of the latitude column
+    - long_col - str - name of the longitude column
+    :output:
+    - pandas.DataFrame - df with a lat, long, and frog_count based on the inputted frog observances
+    """
     df["lat"] = round(df[lat_col],1)
     df["long"] = round(df[long_col],1)
 
@@ -30,6 +52,27 @@ def count_frogs(df, degrees, lat_col, long_col):
     merged_grid = empty_grid.grid.merge(unique_coords, how="left")
     
     return merged_grid
+
+def haversine_formula(coord1, coord2):
+    """
+    Source:
+
+    Determine the distance between two coordinates accoring to the Haversine formula
+    :inputs:
+    - coord1 - list - first coordinates i.e. (lat, long)
+    - coord2 - list - second coordinates i.e. (lat, long)
+    :output:
+    - int - distance in kim  
+    """
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    return c * r
 
 if __name__ == "__main__":
     pass
