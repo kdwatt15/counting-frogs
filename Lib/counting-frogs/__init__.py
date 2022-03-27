4513d23aee10f4b103ee6d786839ec69b42f8d45
Kevin Watt <kevin.watt@ey.com>
2022-03-26 20:36:56 -0500

    cleaning up names

--- Lib/counting-frogs/__init__.py
+++ Lib/counting-frogs/__init__.py
@@ -1,2 +1,1 @@
 # counting-frogs main __init__ file
-from .utils import Grid
--- Lib/counting-frogs/utils/__init__.py
+++ Lib/counting-frogs/utils/__init__.py
@@ -1,3 +1,2 @@
 # utils __init__ file
-from .grid import grid
-from .frog_counter import count_frogs
+from .utils import count_frogs
